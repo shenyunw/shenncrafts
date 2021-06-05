8cf458300a2161f5741608927ee81737557de83d
shenyunw <wangs321@myumanitoba.ca>
2021-06-04 19:23:01 -0500

    rename about me route

--- shennCrafts-app/src/app/app-routing.module.ts
+++ shennCrafts-app/src/app/app-routing.module.ts
@@ -4,13 +4,13 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-    { path: 'home', component: HomeComponent},
-    { path: 'about-me', component: AboutMeComponent},
-    { path: '', redirectTo: '/home', pathMatch: 'full' }
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutMeComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
