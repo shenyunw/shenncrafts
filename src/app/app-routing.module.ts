25053b4bbc71c443e7ca670395cc02808020956d
shenyunw <wangs321@myumanitoba.ca>
2022-01-13 18:36:19 -0600

    feat: add gallery

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { GalleryComponent } from './gallery/gallery.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutMeComponent },
+    { path: 'gallery', component: GalleryComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
